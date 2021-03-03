68eb083bcdd1b6fc89e8274c19dcf74cb5e928e5
Alessandro Scaramuzza <scaramuzzaa@informsrl.it>
2021-03-03 12:26:19 +0100

    font

---
+++ src/com/itskennedy/corsoCloudDeveloper/FontUtils.java
@@ -0,0 +1,10 @@
+package com.itskennedy.corsoCloudDeveloper;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class FontUtils {
+	public static final Font FONT_24 = new JLabel().getFont().deriveFont(24f);
+
+}
