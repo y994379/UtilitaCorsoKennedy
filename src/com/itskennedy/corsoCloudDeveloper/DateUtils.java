ccefc78f22fd8d35fd6425664e5431f68b3d1d1d
Alessandro Scaramuzza <scaramuzzaa@informsrl.it>
2021-03-03 12:51:05 +0100

    sdf

--- src/com/itskennedy/corsoCloudDeveloper/DateUtils.java
+++ src/com/itskennedy/corsoCloudDeveloper/DateUtils.java
@@ -7,5 +7,7 @@ public class DateUtils {
 	public static final SimpleDateFormat SDF_SETTIMANA_ANNO_MESE_GIORNO = new SimpleDateFormat("EEE dd/MMM/yyyy");
 	public static final SimpleDateFormat SDF_ANNO_MESE_GIORNO_ORE_MINUTI_SECONDI = new SimpleDateFormat("dd/MMM/yyyy hh:mm:ss");
 	public static final SimpleDateFormat SDF_ORE_MINUTI_SECONDI = new SimpleDateFormat("hh:mm:ss");
+	public static final SimpleDateFormat SDF_MINUTI_SECONDI_MILLISECONDI = new SimpleDateFormat("mm:ss:SSS");
+	public static final SimpleDateFormat SDF_MINUTI_SECONDI_MILLISECONDI_PARLANTE  = new SimpleDateFormat("mm 'minuti' ss 'secondi' SSS 'milli'");
 
 }
