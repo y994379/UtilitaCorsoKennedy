5ff47c756510bbb84dc5178191881f0672017fba
Alessandro Scaramuzza <scaramuzzaa@informsrl.it>
2021-03-03 12:17:00 +0100

    nuovo dateformat

--- src/com/itskennedy/corsoCloudDeveloper/DateUtils.java
+++ src/com/itskennedy/corsoCloudDeveloper/DateUtils.java
@@ -6,5 +6,6 @@ public class DateUtils {
 	public static final SimpleDateFormat SDF_ANNO_MESE_GIORNO = new SimpleDateFormat("dd/MMM/yyyy");
 	public static final SimpleDateFormat SDF_SETTIMANA_ANNO_MESE_GIORNO = new SimpleDateFormat("EEE dd/MMM/yyyy");
 	public static final SimpleDateFormat SDF_ANNO_MESE_GIORNO_ORE_MINUTI_SECONDI = new SimpleDateFormat("dd/MMM/yyyy hh:mm:ss");
+	public static final SimpleDateFormat SDF_ORE_MINUTI_SECONDI = new SimpleDateFormat("hh:mm:ss");
 
 }
