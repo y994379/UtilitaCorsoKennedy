7f17486482e4a32455c7fda6f461726f47466c79
ITSOngaroT <ITSOngaroT@itscor207393.itsacademy.corvallisholding.it>
2021-03-03 10:51:46 +0100

    commit timothy

--- src/com/itskennedy/corsoCloudDeveloper/FrameCorso.java
+++ src/com/itskennedy/corsoCloudDeveloper/FrameCorso.java
@@ -7,7 +7,7 @@ public class FrameCorso extends JFrame {
 	public FrameCorso() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocation(500, 400);
-		this.setSize(400,300);
+		this.setSize(800,300);
 		this.setTitle("Frame principale dell'applicazione");
 	}
 }
