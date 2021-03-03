cc11584fa133f047da7560901d54e193d04e399f
ITSPartonS <ITSPartonS@itscor207395.itsacademy.corvallisholding.it>
2021-03-03 11:01:47 +0100

    prova

--- src/com/itskennedy/corsoCloudDeveloper/FrameCorso.java
+++ src/com/itskennedy/corsoCloudDeveloper/FrameCorso.java
@@ -6,8 +6,8 @@ public class FrameCorso extends JFrame {
 	
 	public FrameCorso() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocation(500, 400);
-		this.setSize(400,300);
+		this.setLocation(1000, 500);
+		this.setSize(700,700);
 		this.setTitle("Frame principale dell'applicazione");
 	}
 }
